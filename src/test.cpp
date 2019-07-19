1f7a2dc9dbd7158d69fbcad9d12251b3ee31e469
neulii <neulii@gmx.at>
2019-07-19 09:08:39 +0200

    new test is in progress

--- src/test.cpp
+++ src/test.cpp
@@ -1,31 +1,16 @@
 #include <iostream>
 
-class BaseClass{
-	
-	public:
-
-		void showMessage(){
-
-			std::cout << "message from baseclass" << std::endl;
-		}
-
-
-};
-
-class DerivedClass : public BaseClass{
-
-	public:
-		void showMessage(){
-			std::cout << "message from derivedClass" << std::endl;
-		}
-
-};
-
 int main(){
 
-	DerivedClass test;
+	std::cout << "super test";
 
-	test.showMessage();
+	int i = 10;
+
+
+	
+
+
+	
 
 
 

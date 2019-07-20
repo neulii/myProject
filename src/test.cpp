17d1db3ab634041bfa827bfaf279ec1b51090a42
neulii <neulii@gmx.at>
2019-07-20 16:36:00 +0200

    writing prog supi

--- src/test.cpp
+++ src/test.cpp
@@ -5,10 +5,19 @@
 #include <vector>
 #include <string>
 
+//checking cls/clear command for different operating systems
+#if defined _WIN32
+	#define clearScreen "\"cls\""
+#elif defined __APPLE__
+	#define clearScreen "\"clear\""
+#endif
+
+
 using namespace std;
 
 void printMenu();
 void getTextFromUser();
+void showBufferText();
 
 char userInput;
 bool userIsInteracting = true;
@@ -16,34 +25,40 @@ vector<string> stringToWrite;
 
 int main(){
 
-	system("cls");
-	printMenu();
+	system(clearScreen);
+	
 	
 	do{
 
+		printMenu();
 		cin >> userInput;
+		system(clearScreen);
 
 		switch(userInput){
-
-			//write in file
-			case '1':
-				cout << "schreiben" << endl;
-				
-				break;
-			
-			//read from file
-			case '2':
-				getTextFromUser();
-
-				break;
 
 			case '0':
 				cout << "Programm Ende erreicht" << endl;
 				userIsInteracting = false;
 				break;
 
+			//write in file
+			case '1':
+				getTextFromUser();
+				
+				break;
+			
+			//read from file
+			case '2':
+				break;
+
+
+			case '3':
+				showBufferText();
+				cout << endl << endl;
+				break;
+
 			default: 
-				system("cls");
+				system(clearScreen);
 				
 				cout << "Eingabe nicht erkannt" << endl <<endl;
 			
@@ -51,9 +66,7 @@ int main(){
 
 		}
 	}while(userIsInteracting);
-
-
-
+	
 	return 0;
 }
 
@@ -62,6 +75,7 @@ void printMenu(){
 	cout << "Was wollen Sie tun?" << endl<<endl;
 	cout << "1 - Datei schreiben" << endl;
 	cout << "2 - Datei lesen" << endl;
+	cout << "3 - Text im Buffer anzeigen" << endl;
 	cout << "==================" << endl;
 	cout << "0 - Beenden" << endl << endl;
 
@@ -74,18 +88,29 @@ void getTextFromUser(){
 	string lineString = "empty";
 	bool userInputLoop = true;
 
-	cout << "Geben Sie einen Text ein: " << endl << endl;
+	cout << "Geben Sie einen Text ein (Beenden mit Leerzeile): " << endl << endl;
 	
-	cin.clear();
+	//entfernt die letzte eingabetaste
+	getchar();
 	
 	while(userInputLoop){
 		getline(cin, lineString);
 		if(lineString.length()==0){
 			break;
 		}
-	}
-	cout << "end of while" << endl;
+		else {
+			//cout << "eingefuegt" << endl;
+			stringToWrite.push_back(lineString);
 
+		}
 
+	}	
+}
+
+void showBufferText(){
+	cout << "Text im Buffer: " << endl << endl;
 	
+	for(int i = 0; i < stringToWrite.size();i++){
+		cout << stringToWrite.at(i) << endl;
+	}
 }

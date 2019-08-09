37dc82a8fb871b73ce02d5ee7045c58c12f26f27
neulii <neulii@gmx.at>
2019-08-09 12:08:22 +0200

    learning

--- src/test.cpp
+++ src/test.cpp
@@ -1,31 +1,12 @@
 #include <iostream>
+#include <bitset>
 
 #define NL '\n'
 
 
-int getValueFromUser(){
-	int inputNumber = 0; 
-	std::cout << "enter your number: " << NL;
-	std::cin >> inputNumber;
-	//test;
-
-
-	return inputNumber;
-
-}
-
-void showNumber(int number){
-
-	std::cout << "number: " << number << NL;
-
-}
-
 int main(){
 
-	int number = getValueFromUser();
 
-	showNumber(number);
-	 
 	return EXIT_FAILURE;
 	
 }

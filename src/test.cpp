83d2ef35853b480ff59c9cdfb10e93ddc847b083
neulii <neulii@gmx.at>
2019-08-11 01:33:36 +0200

    random generator works

--- src/test.cpp
+++ src/test.cpp
@@ -10,8 +10,16 @@ int main(){
 	std::srand(static_cast<unsigned long>(std::time(nullptr)));
 
 	for (int i = 1; i<=100; i++){
+		
+		const int range = 6;
+		const int begin = 0;
+		int rn = 0;
+		
+		int number = 0;
+		number = std::rand();
 
-		cout << std::rand() << "\t" ;
+		rn = number % (range-begin+1) +begin;
+		cout << rn << "\t";
 
 		if(i%5==0){
 			cout << NL;

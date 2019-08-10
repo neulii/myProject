6221b8f70b09edf3728ee0f4bdf20062c991e67c
neulii <neulii@gmx.at>
2019-08-10 15:46:25 +0200

    testing

--- src/test.cpp
+++ src/test.cpp
@@ -6,6 +6,22 @@
 
 int main(){
 
+	const unsigned char hungry	= 1 << 0; //0000_0001
+	const unsigned char angry 	= 1 << 1; //0000_0010
+	const unsigned char men 	= 1 << 4; //0001_0000
+	const unsigned char woman  	= 1 << 5; //0010_0000
+
+	unsigned char options = (0 | hungry);
+	options = options | angry;
+
+	options = options | woman;
+	
+
+	std::cout << std::bitset<8>(options) << NL;
+
+
+
+
 
 	return EXIT_FAILURE;
 	

4c389e90e3f44d15feed8d5aa06f23ae443e1308
neulii <neulii@gmx.at>
2019-08-12 13:41:19 +0200

    supi

--- src/test.cpp
+++ src/test.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <ctime>
 #include "Library.h"
+#include <algorithm>
 
 #define NL '\n'
 #define TAB '\t'
@@ -11,14 +12,16 @@ int main(){
 
 	int numbers[] = {22,66,10,1,66,4000,100,3,20,444};
 	size_t arraySize = sizeof(numbers)/sizeof(numbers[0]);
-	
 	neulii::printArrayToConsole(numbers, arraySize);
 
-	neulii::sortIntegerArray(numbers,arraySize);
+	std::sort(numbers, numbers+arraySize);
+	
+	//neulii::sortIntegerArray(numbers,arraySize);
 
 	neulii::printArrayToConsole(numbers, arraySize);
 	
 	
+
 	return EXIT_FAILURE;
 	
 }

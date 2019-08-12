97e380075fa35805cd104161d81ae65d36dd56be
neulii <neulii@gmx.at>
2019-08-12 21:54:52 +0200

    array dymnamic

--- src/test.cpp
+++ src/test.cpp
@@ -4,6 +4,7 @@
 #include <algorithm>
 #include <iterator>
 #include <string>
+#include <string>
 
 #define NL '\n'
 #define TAB '\t'
@@ -12,14 +13,40 @@ using std::cout;
 
 int main(){
 
+	std::string* names;
 
-	const char* test = "supertest";
+	unsigned numberOfNames = 0;
+	std::string nameInput;
 
-	cout << test << NL;
+	cout << "How many names to store: ";
+	std::cin >> numberOfNames;
+	std::cin.ignore(10000,'\n');
 
-	*test = "hallo";
+
+	names = new std::string[numberOfNames];
 
 
+	for(unsigned i = 0; i<numberOfNames; i++){
+		cout << "Name " << i << ": ";
+		std::getline(std::cin, names[i]);
+
+
+	}
+
+	cout << NL<< NL;
+	cout << "names you entered:" << NL;
+	cout << "=========================" << NL;
+
+	
+
+
+
+	for (unsigned i = 0; i<numberOfNames; i++){
+		std::cout << "name " << i <<": "<< names[i] << NL;
+
+	}
+	
+	
 
 	return EXIT_FAILURE;
 	
